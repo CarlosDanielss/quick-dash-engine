e0745fcc4bb15f609e10d36dd3bc6705ad0a11e0
Carlos Daniel <carlos.daniel@ostenmoove.com.br>
2025-02-25 19:20:34 -0300

    feat: add export global

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from "./src/core/quick-dash-engine.js";
+export * from "./src/core/quick-dash-parser.js";
+
+export * from "./src/types/query-map.js";
+export * from "./src/types/widget.js";
+export * from "./src/types/variables.js";
+export * from "./src/types/dashboard-template.js";
